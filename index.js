4a493973cefa57f99855c8f4a97f2b096f54d2aa
James Boynton <james.boynton@gmail.com>
2019-04-12 18:44:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ function logWhisper(string) {
 } 
 function sayHiToGrandma(string) {
   return(string.toLowerCase())
-}
-function sayHiToGrandma(string) {
   return(string.toUpperCase())
-}
-function sayHiToGrandma(string) {
   return(string.toLowerCase())
 }
